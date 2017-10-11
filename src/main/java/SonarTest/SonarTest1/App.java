44dd7930bcff8063d1517db395ab85e6a96a1461
vishaljain93 <vishaljain93@hotmail.com>
2017-10-11 17:15:20 +0530

    First Push Try

--- src/main/java/SonarTest/SonarTest1/App.java
+++ src/main/java/SonarTest/SonarTest1/App.java
@@ -1,13 +1,18 @@
 package SonarTest.SonarTest1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        LOGGER.info("Hello Jenkins");
     }
 }
