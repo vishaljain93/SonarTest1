105ee67882fb0d64b5e3641c9907bfa7e3bb9010
vishaljain93 <vishaljain93@hotmail.com>
2017-10-12 11:40:22 +0530

    Third Push Try

--- src/main/java/SonarTest/SonarTest1/App.java
+++ src/main/java/SonarTest/SonarTest1/App.java
@@ -12,7 +12,6 @@ public class App
 	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         LOGGER.info("Hello Jenkins!!!");
     }
 }
